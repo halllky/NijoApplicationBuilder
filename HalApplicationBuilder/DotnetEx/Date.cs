ecb04882fa4092cba52b15f40e281fa22ca01252
halky <yuyami.stars@gmail.com>
2023-01-10 10:02:47 +0900

    Date型を作成

---
+++ HalApplicationBuilder/DotnetEx/Date.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HalApplicationBuilder.DotnetEx {
+    public class Date : ValueObject {
+        public Date(DateTime? value) {
+            Value = value?.Date;
+        }
+
+        public DateTime? Value { get; }
+
+        protected override IEnumerable<object> ValueObjectIdentifiers() {
+            yield return Value;
+        }
+
+        public override string ToString() {
+            return Value?.ToShortDateString();
+        }
+    }
+}
