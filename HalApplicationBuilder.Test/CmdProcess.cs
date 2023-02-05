396276d20e2dc03cc9be2b2575590263f669185a
halky <yuyami.stars@gmail.com>
2023-02-05 17:48:15 +0900

    fix: cannot catch build error

--- HalApplicationBuilder.Test/CmdProcess.cs
+++ HalApplicationBuilder.Test/CmdProcess.cs
@@ -6,42 +6,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HalApplicationBuilder.Test {
-    public class CmdProcess {
-        public CmdProcess(string workingDirectory = null) {
+namespace HalApplicationBuilder.Test
+{
+    public class CmdProcess
+    {
+        public CmdProcess(string workingDirectory = null)
+        {
             WorkingDirectory = workingDirectory;
         }
 
         public string WorkingDirectory { get; }
 
-        public StreamReader Start(string filename, params string[] args) {
-            var startInfo = new ProcessStartInfo {
-                WorkingDirectory = WorkingDirectory,
-                FileName = filename,
-                UseShellExecute = false,
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                StandardErrorEncoding = Encoding.UTF8,
-                StandardOutputEncoding = Encoding.UTF8,
-            };
+        public string Start(string filename, params string[] args)
+        {
+            using var process = new Process();
+            process.StartInfo.WorkingDirectory = WorkingDirectory;
+            process.StartInfo.FileName = filename;
+            foreach (var arg in args) process.StartInfo.ArgumentList.Add(arg);
 
-            foreach (var arg in args) {
-                startInfo.ArgumentList.Add(arg);
-            }
+            process.StartInfo.UseShellExecute = false;
 
-            var process = Process.Start(startInfo);
+            var standardOutput = new StringBuilder();
+            var standardError = new StringBuilder();
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.StartInfo.StandardErrorEncoding = Encoding.UTF8;
+            process.StartInfo.StandardOutputEncoding = Encoding.UTF8;
+            process.OutputDataReceived += (sender, e) => standardOutput.AppendLine(e.Data);
+            process.ErrorDataReceived += (sender, e) => standardError.AppendLine(e.Data);
+
+            Console.WriteLine($"PROCESS START: {filename} {string.Join(" ", args)}");
+
+            process.Start();
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
             process.WaitForExit();
 
-            if (process.ExitCode != 0) {
-                var standardError = process.StandardError.ReadToEnd();
+            Console.WriteLine($"PROCESS END:   {filename} {string.Join(" ", args)}");
+
+            if (process.ExitCode != 0 || standardError.Length > 0)
+            {
                 throw new InvalidOperationException(
                   $"プロセス\"{filename}\"の実行時にエラーが発生しました。" + Environment.NewLine +
                   $"実行時引数: " + string.Join(" ", args) + Environment.NewLine +
                   $"終了コード: " + process.ExitCode.ToString() + Environment.NewLine +
-                  standardError);
+                  $"========== standard output =========" + Environment.NewLine +
+                  standardOutput.ToString() + Environment.NewLine +
+                  $"========== standard error ==========" + Environment.NewLine +
+                  standardError.ToString() + Environment.NewLine +
+                  $"====================================" + Environment.NewLine);
             }
 
-            return process.StandardOutput;
+            return standardOutput.ToString();
         }
     }
 }
