9ae1b6168263c1c4d6793c1152835567f1da1cfa
halky <yuyami.stars@gmail.com>
2023-04-23 18:17:35 +0900

    add feature to change server side url in client

--- HalApplicationBuilder/CodeRendering/ReactAndWebApi/project-template/src/__AutoGenerated/components/Dashboard.tsx
+++ HalApplicationBuilder/CodeRendering/ReactAndWebApi/project-template/src/__AutoGenerated/components/Dashboard.tsx
@@ -1,18 +1,30 @@
 import React, { useCallback } from 'react';
+import { useAppContext } from '../hooks/AppContext';
 import { IconButton } from './IconButton';
 
 export const Dashboard = () => {
-    const recreateDatabase = useCallback(() => {
-        if (window.confirm('DBを再作成します。データは全て削除されます。よろしいですか？')) {
-            fetch(`https://localhost:7275/HalappDebug/recreate-database`, {
-                method: 'PUT',
-            })
-        }
-    }, [])
-    return (
-        <div>
-            top page
-            <IconButton onClick={recreateDatabase}>DB再作成</IconButton>
-        </div>
-    )
+
+  const [{ apiDomain }, dispatch] = useAppContext()
+
+  const recreateDatabase = useCallback(() => {
+    if (window.confirm('DBを再作成します。データは全て削除されます。よろしいですか？')) {
+      fetch(`${apiDomain}/HalappDebug/recreate-database`, {
+        method: 'PUT',
+      }).then(async response => {
+        dispatch({ type: 'pushMsg', msg: await response.text() })
+      })
+    }
+  }, [apiDomain])
+
+  return (
+    <div>
+      <label className="flex flex-row space-x-2">
+        <span className="select-none opacity-50">
+          API DOMAIN
+        </span>
+        <input type="text" value={apiDomain} onChange={e => dispatch({ type: 'changeDomain', value: e.target.value })} className="border" />
+      </label>
+      <IconButton onClick={recreateDatabase}>DB再作成</IconButton>
+    </div>
+  )
 }
