aaa8b14db89386f784a1355454fb1908bc09c6af
halky <yuyami.stars@gmail.com>
2023-06-18 14:08:41 +0900

    wip 残タスク全量把握のため自動テストの整備

---
+++ HalApplicationBuilder.Test/TestArgs.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace HalApplicationBuilder.Test {
+    public class TestArgs {
+        public TestArgs(string xmlFilePath) {
+            _xmlFilePath = xmlFilePath;
+        }
+        private readonly string _xmlFilePath;
+
+        public string LoadXmlString() {
+            return File.ReadAllText(_xmlFilePath).Trim();
+        }
+        public XDocument LoadXDocument() {
+            return XDocument.Parse(LoadXmlString());
+        }
+        public GeneratedProject CreateProject() {
+            using var tokenSource = new CancellationTokenSource();
+            var project = GeneratedProject.Create(
+                applicationName: LoadXDocument().Root!.Name.LocalName,
+                verbose: false,
+                keepTempIferror: false,
+                cancellationToken: tokenSource.Token,
+                log: Console.Out);
+
+            var xmlPath = project.GetAggregateSchemaPath();
+            File.WriteAllText(xmlPath, LoadXmlString());
+
+            project.StartSetup(false, tokenSource.Token)
+                   .EnsureCreateRuntimeSettingFile();
+
+            return project;
+        }
+    }
+}
---
+++ HalApplicationBuilder.Test/Tests/ビルド.cs
@@ -0,0 +1,20 @@
+using HalApplicationBuilder.CodeRendering20230514;
+using HalApplicationBuilder.DotnetEx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Xunit;
+
+namespace HalApplicationBuilder.Test.Tests {
+    public class ビルド {
+        [Theory]
+        [ClassData(typeof(TestArgsBuilder))]
+        public void Test(TestArgs args) {
+            var project = args.CreateProject();
+            project.Build();
+        }
+    }
+}
