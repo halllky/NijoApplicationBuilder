0e6272b5d4249edfd80b457ec833a263cab28f21
halky <yuyami.stars@gmail.com>
2023-10-15 11:54:23 +0900

    useHttpRequest: エラーメッセージを多少わかりやすく

--- HalApplicationBuilder/client/src/__autoGenerated/hooks/useHttpRequest.ts
+++ HalApplicationBuilder/client/src/__autoGenerated/hooks/useHttpRequest.ts
@@ -3,13 +3,52 @@ import { UUID } from "uuidjs"
 import { useAppContext } from "./AppContext"
 import { BarMessage } from "../components/InlineMessageBar"
 
-type HttpGetResult<T> = { ok: true, data: T } | { ok: false, errors: BarMessage[] }
-type HttpPostResult<T = object> = { ok: true, data: T } | { ok: false, errors: BarMessage[] }
+type HttpSendResult<T> = { ok: true, data: T } | { ok: false, errors: BarMessage[] }
 
 export const useHttpRequest = () => {
   const [{ apiDomain }, dispatch] = useAppContext()
 
-  const get = useCallback(async <T = object>(url: string, param: { [key: string]: unknown } = {}): Promise<HttpGetResult<T>> => {
+  const sendHttpRequest = useCallback(async <T>([url, option]: Parameters<typeof fetch>): Promise<HttpSendResult<T>> => {
+    try {
+      const response = await fetch(url, option)
+      if (response.ok) {
+        const text = await response.text()
+        const data = JSON.parse(text) as T
+        return { ok: true, data }
+      } else {
+        dispatch({ type: 'pushMsg', msg: `ERROR(${url})` })
+
+        // エラーメッセージの抽出
+        let errorMessages: string[]
+        const text = await response.text()
+        try {
+          const parsed = JSON.parse(text)
+          if (typeof parsed.content === 'string') {
+            errorMessages = [parsed.content]
+          } else if (Array.isArray(parsed.content)) {
+            errorMessages = parsed.content as string[]
+          } else if (Array.isArray(parsed.errors)) {
+            errorMessages = parsed.errors as string[]
+          } else if (typeof parsed.errors === 'object') {
+            errorMessages = Object.values(parsed.errors).flatMap(err => err as string[])
+          } else {
+            console.log(parsed)
+            errorMessages = [text]
+          }
+        } catch {
+          errorMessages = [text]
+        }
+        const errors: BarMessage[] = errorMessages.map(text => ({ uuid: UUID.generate(), text }))
+        return { ok: false, errors }
+      }
+
+    } catch (error) {
+      dispatch({ type: 'pushMsg', msg: `${error}(${url})` })
+      return { ok: false, errors: [] }
+    }
+  }, [dispatch])
+
+  const get = useCallback(async <T = object>(url: string, param: { [key: string]: unknown } = {}): Promise<HttpSendResult<T>> => {
     if (!apiDomain) return { ok: false, errors: [{ uuid: UUID.generate(), text: 'サーバーが設定されていません。' }] }
     const query = new URLSearchParams()
     for (const key of Object.keys(param)) {
@@ -22,42 +61,16 @@ export const useHttpRequest = () => {
       query.append(key, value)
     }
     const queryString = query.toString()
-    const fullUrl = queryString ? `${apiDomain}${url}?${queryString}` : `${apiDomain}${url}`
-    const response = await fetch(fullUrl)
-    if (response.ok) {
-      const text = await response.text()
-      const data = JSON.parse(text) as T
-      return { ok: true, data }
-    } else {
-      dispatch({ type: 'pushMsg', msg: `ERROR(${fullUrl})` })
-      const text = await response.text()
-      const res: { content: string } = JSON.parse(text)
-      const content: string[] = res.content ? JSON.parse(res.content) : [text]
-      const errors: BarMessage[] = content.map(text => ({ uuid: UUID.generate(), text }))
-      return { ok: false, errors }
-    }
-  }, [apiDomain, dispatch])
+    return await sendHttpRequest([queryString ? `${apiDomain}${url}?${queryString}` : `${apiDomain}${url}`])
+  }, [apiDomain, sendHttpRequest])
 
-  const post = useCallback(async <T>(url: string, data: object = {}): Promise<HttpPostResult<T>> => {
+  const post = useCallback(async <T>(url: string, data: object = {}): Promise<HttpSendResult<T>> => {
     if (!apiDomain) return { ok: false, errors: [{ uuid: UUID.generate(), text: 'サーバーが設定されていません。' }] }
-    const fullUrl = `${apiDomain}${url}`
-    const response = await fetch(fullUrl, {
+    return await sendHttpRequest([`${apiDomain}${url}`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(data),
-    })
-    if (response.ok) {
-      const text = await response.text()
-      const data = JSON.parse(text) as T
-      return { ok: true, data }
-    } else {
-      dispatch({ type: 'pushMsg', msg: `ERROR(${fullUrl})` })
-      const text = await response.text()
-      const res: { content: string } = JSON.parse(text)
-      const content: string[] = res.content ? JSON.parse(res.content) : [text]
-      const errors: BarMessage[] = content.map(text => ({ uuid: UUID.generate(), text }))
-      return { ok: false, errors }
-    }
+    }])
   }, [apiDomain, dispatch])
 
   return { get, post }
