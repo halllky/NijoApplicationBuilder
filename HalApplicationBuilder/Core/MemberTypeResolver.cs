83762f08ee597ffa5ba8441621c915dc03e136b4
halky <yuyami.stars@gmail.com>
2023-11-16 21:10:26 +0900

    Add datetime type

--- HalApplicationBuilder/Core/MemberTypeResolver.cs
+++ HalApplicationBuilder/Core/MemberTypeResolver.cs
@@ -14,7 +14,8 @@ namespace HalApplicationBuilder.Core {
                 .Register(TYPE_WORD, new Word())
                 .Register(TYPE_SENTENCE, new Sentence())
                 .Register(TYPE_INT, new Integer())
-                .Register(TYPE_BOOL, new AggregateMemberTypes.Boolean());
+                .Register(TYPE_BOOL, new AggregateMemberTypes.Boolean())
+                .Register(TYPE_DATETIME, new TimePoint());
             return resolver;
         }
         internal const string TYPE_ID = "id";
@@ -23,6 +24,7 @@ namespace HalApplicationBuilder.Core {
         internal const string TYPE_SENTENCE = "sentence";
         internal const string TYPE_INT = "int";
         internal const string TYPE_BOOL = "bool";
+        internal const string TYPE_DATETIME = "datetime";
 
         private MemberTypeResolver() { }
         private readonly Dictionary<string, IAggregateMemberType> _registered = new();
