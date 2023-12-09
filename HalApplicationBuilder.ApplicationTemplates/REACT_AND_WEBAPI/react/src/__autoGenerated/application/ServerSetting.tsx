63548b63cc42761387cdae2b7a130cc36f4821d4
halky <yuyami.stars@gmail.com>
2023-12-09 11:11:00 +0900

    サーバー設定画面が動かないのを修正

--- HalApplicationBuilder.ApplicationTemplates/REACT_AND_WEBAPI/react/src/__autoGenerated/application/ServerSetting.tsx
+++ HalApplicationBuilder.ApplicationTemplates/REACT_AND_WEBAPI/react/src/__autoGenerated/application/ServerSetting.tsx
@@ -62,74 +62,36 @@ export const ServerSettingScreen = () => {
 
   return (
     <div className="page-content-root">
-
       <VForm.Root>
         <VForm.Section label="基本設定" table>
           <VForm.Row label="APIサーバーURL">
-            <Input.Word {...registerEx(`apiDomain`)} />
+            <Input.Word
+              value={appState.apiDomain}
+              onChange={v => dispatch({ type: 'changeDomain', value: v ?? '' })}
+            />
           </VForm.Row>
           <VForm.Row label="ダークモード">
-            <Input.CheckBox {...registerEx(`darkMode`)} />
-          </VForm.Row>
-          <VForm.Row fullWidth>
-            <Input.IconButton underline icon={ArrowPathIcon} onClick={reload}>保存</Input.IconButton>
+            <Input.CheckBox
+              value={appState.darkMode}
+              onChange={() => dispatch({ type: 'toggleDark' })}
+            />
           </VForm.Row>
         </VForm.Section>
 
         <VForm.Spacer />
 
         {process.env.NODE_ENV === 'development' && (
-          <VForm.Section label="データベース設定" table>
-            <VForm.Row fullWidth>
-              <Input.IconButton underline icon={ArrowPathIcon} onClick={reload}>再読み込み</Input.IconButton>
-              <Input.IconButton fill onClick={recreateDatabase}>DB再作成</Input.IconButton>
+          <VForm.Section label="データベース" table>
+            <VForm.Row hidden={commandErrors.length === 0}>
               <InlineMessageBar value={commandErrors} onChange={setCommandErrors} />
+            </VForm.Row>
+            <VForm.Row fullWidth>
+              <Input.IconButton fill onClick={recreateDatabase}>DB再作成</Input.IconButton>
             </VForm.Row>
           </VForm.Section>
         )}
 
       </VForm.Root>
-
-      ------------------------------------------------------
-
-      {/* {process.env.NODE_ENV === 'development' &&
-        <SettingSection
-          title="データベース"
-          sholder={<IconButton underline icon={ArrowPathIcon} onClick={reload}>再読み込み</IconButton>}
-        >
-          <InlineMessageBar value={settingErrors} onChange={setSettingErrors} />
-          <form onSubmit={handleSubmit(onSaveSettings, onError)}>
-            <Setting
-              label={<div className="flex items-center">
-                接続先DB
-                <IconButton underline icon={PlusIcon} onClick={e => { append(e); e.preventDefault() }} className="self-start inline-flex ml-1">追加</IconButton>
-              </div>}
-              multiRow
-            >
-              <div className="flex-1 flex flex-col items-stretch space-y-1">
-                {fields.map((field, index) => (
-                  <div key={field.id} className="flex items-center space-x-1">
-                    <input {...register(`currentDb`)} type="radio" value={index} />
-                    <Word {...register(`db.${index}.name`, { required: true })} className="basis-32 min-w-0" />
-                    <Word {...register(`db.${index}.connStr`, { required: true })} className="flex-1" />
-                    <IconButton underline icon={XMarkIcon} onClick={e => { remove(index); e.preventDefault() }}>削除</IconButton>
-                  </div>
-                ))}
-
-              </div>
-            </Setting>
-            <IconButton fill icon={BookmarkSquareIcon} className="mt-2">保存</IconButton>
-          </form>
-        </SettingSection>}
-
-      {process.env.NODE_ENV === 'development' &&
-        <SettingSection title="デバッグ用コマンド（開発環境でのみ有効）">
-          <InlineMessageBar value={commandErrors} onChange={setCommandErrors} />
-          <Setting label="DB再作成">
-            <IconButton fill onClick={recreateDatabase}>実行</IconButton>
-          </Setting>
-        </SettingSection>} */}
-
     </div>
   )
 }
