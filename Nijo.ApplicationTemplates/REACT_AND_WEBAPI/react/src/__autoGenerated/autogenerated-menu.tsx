56a154d06fdcf5747547805d64984dc0335b4120
halky <yuyami.stars@gmail.com>
2024-01-14 09:35:09 +0900

    refactor

---
+++ Nijo.ApplicationTemplates/REACT_AND_WEBAPI/react/src/__autoGenerated/autogenerated-menu.tsx
@@ -0,0 +1,7 @@
+/**
+ * このファイルはソース自動生成によって上書きされます。
+ */
+
+export const THIS_APPLICATION_NAME = 'サンプルアプリケーション' as const
+export const routes: { url: string, el: JSX.Element }[] = []
+export const menuItems: { url: string, text: string }[] = []
--- Nijo.ApplicationTemplates/REACT_AND_WEBAPI/react/src/__autoGenerated/index.ts
+++
@@ -1,9 +0,0 @@
-export * from './menu'
-export * from './application'
-export * from './collection'
-export * from './input'
-export * from './util'
-
-import './nijo.css'
-
-export const THIS_APPLICATION_NAME = 'サンプルアプリケーション' as const
