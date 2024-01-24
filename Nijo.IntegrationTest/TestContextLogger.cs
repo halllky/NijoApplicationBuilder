72addeb222e7f68a77acf60d98c9ab59ee6fe0a6
halky <yuyami.stars@gmail.com>
2024-01-25 03:46:39 +0900

    TestContextLoggerでBetginScopeが使えるように

--- Nijo.IntegrationTest/TestContextLogger.cs
+++ Nijo.IntegrationTest/TestContextLogger.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.Logging;
+using Nijo.Util.DotnetEx;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,14 +8,32 @@ using System.Threading.Tasks;
 
 namespace Nijo.IntegrationTest {
     internal class TestContextLogger : ILogger {
-        public IDisposable? BeginScope<TState>(TState state) where TState : notnull {
-            return null;
-        }
+
         public bool IsEnabled(LogLevel logLevel) {
             return true;
         }
+
         public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter) {
-            TestContext.WriteLine($"{DateTime.Now:g}\t[{logLevel}]\t{formatter(state, exception)}");
+            var scope = string.Concat(_scope.Reverse().Select(x => $"{x} => "));
+            TestContext.WriteLine($"{DateTime.Now:g}\t[{logLevel}]\t{scope}{formatter(state, exception)}");
         }
+
+        #region スコープ
+        public IDisposable? BeginScope<TState>(TState state) where TState : notnull {
+            _scope.Push(state.ToString() ?? string.Empty);
+            return new Scope(this);
+        }
+        private class Scope(TestContextLogger owner) : IDisposable {
+            private readonly TestContextLogger _owner = owner;
+            private bool _disposed;
+
+            public void Dispose() {
+                if (_disposed) return;
+                _owner._scope.Pop();
+                _disposed = true;
+            }
+        }
+        private readonly Stack<string> _scope = new();
+        #endregion スコープ
     }
 }
