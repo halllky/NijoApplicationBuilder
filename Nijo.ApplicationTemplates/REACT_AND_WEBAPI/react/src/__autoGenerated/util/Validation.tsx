44480b20161f09ed702a18b5322de803e7d4c898
halky <yuyami.stars@gmail.com>
2024-02-12 11:19:24 +0900

    comment

--- Nijo.ApplicationTemplates/REACT_AND_WEBAPI/react/src/__autoGenerated/util/Validation.tsx
+++ Nijo.ApplicationTemplates/REACT_AND_WEBAPI/react/src/__autoGenerated/util/Validation.tsx
@@ -63,6 +63,7 @@ export const useFormEx = <T extends FieldValues = FieldValues>(props: UseFormExP
   return {
     ...useFormReturns,
     registerEx: <TFieldName extends FieldPath<T>>(name: TFieldName) => ({
+      // TODO: watchを使うとページ全体に再レンダリングが走ってしまう
       value: useFormReturns.watch(name),
       onChange: (value: PathValue<T, TFieldName>) => {
         useFormReturns.setValue(name, value)
@@ -76,6 +77,7 @@ export const useFormContextEx = <T extends FieldValues = FieldValues>() => {
   return {
     ...useFormContextReturns,
     registerEx: <TFieldName extends FieldPath<T>>(name: TFieldName) => ({
+      // TODO: watchを使うとページ全体に再レンダリングが走ってしまう
       value: useFormContextReturns.watch(name),
       onChange: (value: PathValue<T, TFieldName>) => {
         useFormContextReturns.setValue(name, value)
