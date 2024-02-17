fe22ab3ed81d0f148d5020013c4196bebab994ac
halky <yuyami.stars@gmail.com>
2024-02-17 16:08:04 +0900

    プロジェクト新規作成処理のテストのたびに大量の通信が発生するのを抑える

--- Nijo.IntegrationTest/Perspectives/プロジェクト新規作成処理のテスト.cs
+++ Nijo.IntegrationTest/Perspectives/プロジェクト新規作成処理のテスト.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Nijo.Util.DotnetEx;
 using System;
 using System.Collections.Generic;
@@ -31,6 +32,10 @@ namespace Nijo.IntegrationTest.Perspectives {
 
             var services = new ServiceCollection();
             GeneratedProject.ConfigureDefaultServices(services);
+            services.AddTransient<IPackageInstaller>(_ => {
+                return new PackageInstallerForCreateTest(logger);
+            });
+
             var serviceProvider = services.BuildServiceProvider();
 
             //// nijo create コマンドを実行
@@ -73,6 +78,89 @@ namespace Nijo.IntegrationTest.Perspectives {
             // DB作成が正常終了していればダミーデータ4個分のリンクがあるはず
             webDriver.FindElement(Util.ByInnerText("集約A")).Click();
             Assert.That(webDriver.FindElements(Util.ByInnerText("詳細")).Count, Is.EqualTo(4));
+        }
+
+
+        /// <summary>
+        /// 新規作成処理のたびにnpn ci による大量のパッケージのインストールが発生して
+        /// 通信量を圧迫するのを防ぐため、ローカルにインストール済みのnode_modulesを利用するための仕組み
+        /// </summary>
+        private class PackageInstallerForCreateTest : IPackageInstaller {
+            public PackageInstallerForCreateTest(ILogger logger) {
+                _logger = logger;
+            }
+            private readonly ILogger _logger;
+
+            public async Task InstallDependencies(GeneratedProject project, CancellationToken cancellationToken) {
+
+                var reactTemplateDir = Path.GetFullPath(Path.Combine(
+                    TestContext.CurrentContext.WorkDirectory,
+                    "..",
+                    "..",
+                    "..",
+                    "..",
+                    "Nijo.ApplicationTemplates",
+                    "REACT_AND_WEBAPI",
+                    "react"));
+
+                var nodeModules = Path.GetFullPath(Path.Combine(
+                    project.WebClientProjectRoot,
+                    "node_modules"));
+
+                if (Directory.Exists(nodeModules)) {
+                    _logger.LogInformation("node_modulesフォルダが既に存在するためインストールをスキップします。");
+
+                } else if (NodeModulesIsInstalled(reactTemplateDir)) {
+
+                    // reactテンプレートのnode_modulesをコピーする
+                    var reactTemplateDirNodeModules = Path.GetFullPath(Path.Combine(
+                        reactTemplateDir,
+                        "node_modules"));
+                    _logger.LogInformation("npm ci のかわりに右記ディレクトリからのコピーを行います: {0}", reactTemplateDirNodeModules);
+
+                    var process = new Process();
+                    process.StartInfo.FileName = "robocopy";
+                    process.StartInfo.ArgumentList.Add("/S");
+                    process.StartInfo.ArgumentList.Add("/NFL"); // No File List
+                    process.StartInfo.ArgumentList.Add("/NDL"); // No Directory List
+                    process.StartInfo.ArgumentList.Add("/NJH"); // No Job Header
+                    process.StartInfo.ArgumentList.Add("/NJS"); // No Job Summary
+                    process.StartInfo.ArgumentList.Add(reactTemplateDirNodeModules);
+                    process.StartInfo.ArgumentList.Add(nodeModules);
+
+                    process.Start();
+
+                    await process.WaitForExitAsync(TestContext.CurrentContext.CancellationToken);
+
+                    // 戻り値1が正常終了
+                    if (process.ExitCode != 1) {
+                        throw new InvalidOperationException("robocopyが終了コード1以外で終了しました。");
+                    }
+
+                    _logger.LogInformation("コピーを完了しました。");
+
+                } else {
+                    // インストール済みでない場合は通常通り npm ci する
+                    var defaultInstaller = new PackageInstaller();
+                    await defaultInstaller.InstallDependencies(project, cancellationToken);
+                }
+            }
+
+            private static bool NodeModulesIsInstalled(string dir) {
+
+                // 念のため全く違うディレクトリのnode_modulesの有無を確認しようとしていないかを確認
+                var packageJson = Path.Combine(dir, "package.json");
+                if (!File.Exists(packageJson))
+                    throw new InvalidOperationException($"Reactテンプレートプロジェクトではない場所のnode_moduleの有無を確認しようとしています: {dir}");
+
+                var nodeModules = new DirectoryInfo(Path.Combine(dir, "node_modules"));
+                if (!nodeModules.Exists) return false;
+
+                var anyPackage = nodeModules.GetDirectories("*", SearchOption.TopDirectoryOnly).Any();
+                if (!anyPackage) return false;
+
+                return true;
+            }
         }
     }
 }
