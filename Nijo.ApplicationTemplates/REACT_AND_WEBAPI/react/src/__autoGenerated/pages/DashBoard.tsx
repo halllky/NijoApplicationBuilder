9d146b969160e52ee7bfcab9fa46e4beabcaa34b
halky <yuyami.stars@gmail.com>
2024-02-23 11:01:38 +0900

    DB再作成ボタンをトップページに移動

---
+++ Nijo.ApplicationTemplates/REACT_AND_WEBAPI/react/src/__autoGenerated/pages/DashBoard.tsx
@@ -0,0 +1,51 @@
+import { useCallback, useState } from 'react'
+import { useHttpRequest, useMsgContext, useDummyDataGenerator } from '../util'
+import * as Input from '../input'
+import { VerticalForm as VForm } from '../collection'
+
+/** DashBoard */
+export default function ({ applicationName }: {
+  applicationName?: string
+}) {
+
+  // デバッグ用DB再作成コマンド
+  const [, dispatchMsg] = useMsgContext()
+  const { post } = useHttpRequest()
+  const [withDummyData, setWithDummyData] = useState<boolean | undefined>(true)
+  const genereateDummyData = useDummyDataGenerator()
+  const recreateDatabase = useCallback(async () => {
+    if (window.confirm('DBを再作成します。データは全て削除されます。よろしいですか？')) {
+      const response = await post('/WebDebugger/recreate-database')
+      if (!response.ok) { return }
+      if (withDummyData) {
+        const success = await genereateDummyData()
+        if (!success) {
+          dispatchMsg(msg => msg.error('DBを再作成しましたがダミーデータ作成に失敗しました。'))
+          return
+        }
+      }
+      dispatchMsg(msg => msg.info('DBを再作成しました。'))
+    }
+  }, [post, withDummyData, genereateDummyData, dispatchMsg])
+
+  return (
+    <div className="page-content-root">
+      <h1 className="p-1 text-lg font-semibold select-none">
+        {applicationName}
+      </h1>
+
+      <hr />
+
+      {import.meta.env.DEV && (
+        <VForm.Root className="p-4">
+          <VForm.Section label="デバッグ用コマンド（開発環境でのみ有効）" table>
+            <VForm.Row fullWidth>
+              <Input.Button outlined onClick={recreateDatabase}>DBを再作成する</Input.Button>
+              <Input.CheckBox value={withDummyData} onChange={setWithDummyData}>ダミーデータも併せて作成する</Input.CheckBox>
+            </VForm.Row>
+          </VForm.Section>
+        </VForm.Root>
+      )}
+    </div>
+  )
+}
--- Nijo.ApplicationTemplates/REACT_AND_WEBAPI/react/src/__autoGenerated/util/UserSetting.tsx
+++ Nijo.ApplicationTemplates/REACT_AND_WEBAPI/react/src/__autoGenerated/util/UserSetting.tsx
@@ -1,11 +1,8 @@
-import { useCallback, useMemo, useState } from 'react';
+import { useMemo } from 'react';
 import * as Input from "../input";
 import { VerticalForm as VForm } from "../collection";
 import { useFormEx } from "./ReactHookFormUtil";
-import { useMsgContext } from './Notification'
-import { useHttpRequest } from './Http'
 import { defineStorageContext } from './Storage'
-import { useDummyDataGenerator } from './useDummyDataGenerator'
 
 export type UserSettings = {
   apiDomain?: string
@@ -31,28 +28,7 @@ export const [UserSettingContextProvider, useUserSetting] = defineStorageContext
 export const ServerSettingScreen = () => {
 
   const { data: appState, save } = useUserSetting()
-  const [, dispatchMsg] = useMsgContext()
   const { registerEx, handleSubmit } = useFormEx<UserSettings>({ defaultValues: appState })
-
-  // --------------------------------------
-  // DB再作成
-  const { post } = useHttpRequest()
-  const [withDummyData, setWithDummyData] = useState<boolean | undefined>(true)
-  const genereateDummyData = useDummyDataGenerator()
-  const recreateDatabase = useCallback(async () => {
-    if (window.confirm('DBを再作成します。データは全て削除されます。よろしいですか？')) {
-      const response = await post('/WebDebugger/recreate-database')
-      if (!response.ok) { return }
-      if (withDummyData) {
-        const success = await genereateDummyData()
-        if (!success) {
-          dispatchMsg(msg => msg.error('DBを再作成しましたがダミーデータ作成に失敗しました。'))
-          return
-        }
-      }
-      dispatchMsg(msg => msg.info('DBを再作成しました。'))
-    }
-  }, [post, withDummyData, genereateDummyData, dispatchMsg])
 
   return (
     <form className="page-content-root" onSubmit={handleSubmit(save)}>
@@ -86,18 +62,6 @@ export const ServerSettingScreen = () => {
             <Input.Button outlined submit>更新</Input.Button>
           </VForm.Row>
         </VForm.Section>
-
-        <VForm.Spacer />
-
-        {process.env.NODE_ENV === 'development' && (
-          <VForm.Section label="データベース" table>
-            <VForm.Row fullWidth>
-              <Input.Button outlined onClick={recreateDatabase}>DB再作成</Input.Button>
-              <Input.CheckBox value={withDummyData} onChange={setWithDummyData}>ダミーデータも併せて作成する</Input.CheckBox>
-            </VForm.Row>
-          </VForm.Section>
-        )}
-
       </VForm.Root>
     </form>
   )
