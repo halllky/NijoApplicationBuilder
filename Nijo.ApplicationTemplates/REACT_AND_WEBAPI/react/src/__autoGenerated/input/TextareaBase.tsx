2a1aae9c46bb64a9f5330b6b144fe5f970a48205
halky <yuyami.stars@gmail.com>
2024-02-25 20:35:33 +0900

    Use react-simplemde-editor

--- Nijo.ApplicationTemplates/REACT_AND_WEBAPI/react/src/__autoGenerated/input/TextareaBase.tsx
+++ Nijo.ApplicationTemplates/REACT_AND_WEBAPI/react/src/__autoGenerated/input/TextareaBase.tsx
@@ -1,7 +1,9 @@
-import React, { TextareaHTMLAttributes, useCallback, useImperativeHandle, useRef } from "react"
-import { defineCustomComponent } from "./InputBase"
+import React, { TextareaHTMLAttributes, useCallback, useImperativeHandle, useRef } from 'react'
+import SimpleMDE from 'react-simplemde-editor'
+import 'easymde/dist/easymde.min.css'
+import { defineCustomComponent } from './InputBase'
 
-export const TextareaBase = defineCustomComponent<string, {}, TextareaHTMLAttributes<HTMLTextAreaElement>>((props, ref) => {
+export const TextareaBase = defineCustomComponent<string, {}, TextareaHTMLAttributes<HTMLDivElement>>((props, ref) => {
 
   const {
     onChange,
@@ -13,35 +15,43 @@ export const TextareaBase = defineCustomComponent<string, {}, TextareaHTMLAttrib
     ...rest
   } = props
 
-  const textareaRef = useRef<HTMLTextAreaElement>(null)
+  const divRef = useRef<HTMLDivElement>(null)
 
   useImperativeHandle(ref, () => ({
-    getValue: () => textareaRef.current?.value,
-    focus: () => textareaRef.current?.focus(),
-  }), [])
+    getValue: () => value,
+    focus: () => divRef.current?.focus(),
+  }), [value])
 
-  const handleFocus: React.FocusEventHandler<HTMLTextAreaElement> = useCallback(e => {
-    if (readOnly) textareaRef.current?.select()
+  const handleFocus: React.FocusEventHandler<HTMLDivElement> = useCallback(e => {
     onFocus?.(e)
   }, [onFocus, readOnly])
-  const onTextChange: React.ChangeEventHandler<HTMLTextAreaElement> = useCallback(e => {
-    onChange?.(e.target.value)
+  const onTextChange = useCallback((value: string) => {
+    onChange?.(value)
   }, [onChange])
 
+  // TODO: ver0.1.1現在はreadonlyになることは無いので適当に実装している
+  if (readOnly) return (
+    <span>
+      {value}
+    </span>
+  )
+
   return (
-    <textarea
-      {...rest}
-      ref={textareaRef}
+    <SimpleMDE
+      ref={divRef}
       value={value ?? ''}
-      className={readOnly
-        ? `block w-full outline-none px-1 border border-color-4 bg-transparent cursor-default ${className}`
-        : `block w-full outline-none px-1 border border-color-5 bg-color-base  ${className}`}
-      autoComplete="off"
-      spellCheck={false}
-      rows={rows || 3}
-      readOnly={readOnly}
-      onFocus={handleFocus}
       onChange={onTextChange}
+      className={`w-full ${className}`}
+      onFocus={handleFocus}
+      spellCheck="false"
+      options={OPTIONS}
+      {...rest}
     />
   )
 })
+
+const OPTIONS: EasyMDE.Options = {
+  spellChecker: false, // trueだと日本語の部分が全部チェックに引っかかってしまう
+  toolbar: [], // ツールバー全部非表示
+  minHeight: '3rem',
+}
