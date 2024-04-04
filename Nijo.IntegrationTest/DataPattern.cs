780b5f810237d99c22dae0d70413619f65de7f7c
halky <yuyami.stars@gmail.com>
2024-04-04 19:28:48 +0900

    Revert "未作成機能のテストを削除（#3で作成予定）"
    This reverts commit 099e9a5c4c655e04d48b433574accf65cc92c917.

--- Nijo.IntegrationTest/DataPattern.cs
+++ Nijo.IntegrationTest/DataPattern.cs
@@ -22,6 +22,7 @@ namespace Nijo.IntegrationTest {
         public const string FILENAME_011 = "011_ダブル.xml";
         public const string FILENAME_012 = "012_スカラメンバー網羅.xml";
         public const string FILENAME_013 = "013_主キーにRef.xml";
+        public const string FILENAME_014 = "014_バックグラウンドタスク最小限構成.xml";
         public const string FILENAME_015 = "015_ChildrenがDataTableで表示できない.xml";
         public const string FILENAME_016 = "016_NameLike.xml";
         public const string FILENAME_100 = "100_RDRA.xml";
