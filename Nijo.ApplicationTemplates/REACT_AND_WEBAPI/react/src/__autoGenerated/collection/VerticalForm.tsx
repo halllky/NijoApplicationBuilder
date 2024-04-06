a9a9d3078ac469c4dbde34a751e5e0abbc8d39c1
halky <yuyami.stars@gmail.com>
2024-04-06 17:37:06 +0900

    layout 新しいVFormへの置き換え

--- Nijo.ApplicationTemplates/REACT_AND_WEBAPI/react/src/__autoGenerated/collection/VerticalForm.tsx
+++ Nijo.ApplicationTemplates/REACT_AND_WEBAPI/react/src/__autoGenerated/collection/VerticalForm.tsx
@@ -1,207 +1,117 @@
-import React, { HTMLAttributes, useContext, useMemo } from "react";
-import { forwardRefEx } from "../util/ReactUtil";
+import React from "react"
 
-const Root = forwardRefEx<HTMLDivElement, HTMLAttributes<HTMLDivElement> & RootContextValue & {
-  label?: React.ReactNode
-}>((props, ref) => {
-  const {
-    label,
-    children,
-    indentSizePx,
-    leftColumnWidth,
-    ...rest
-  } = props
-
-  const contextValue: RootContextValue = useMemo(() => ({
-    leftColumnWidth: leftColumnWidth ?? '240px',
-    indentSizePx: indentSizePx ?? 24,
-  }), [leftColumnWidth, indentSizePx])
-  const nestedContextValue: NearestSectionContextValue = useMemo(() => ({
-    depth: 0,
-    tableStartDepth: undefined,
-  }), [])
-
-  return (
-    <FormLayoutContext.Provider value={contextValue}>
-      <NearestSectionContext.Provider value={nestedContextValue}>
-        <div ref={ref} {...rest} className={`flex flex-col items-stretch ${rest.className}`}>
-          {label && typeof label === 'string' && (
-            <div className="font-bold mb-2">
-              {label}
-            </div>
-          )}
-          {label && typeof label !== 'string' && label}
-          {children}
-        </div>
-      </NearestSectionContext.Provider>
-    </FormLayoutContext.Provider>
-  )
-})
-
-
-const Section = ((props: {
-  label?: React.ReactNode
-  table?: boolean
+/** 0個以上のItemまたはContainerを内部に含む入れ物。Containerに内包された要素はインデントが1段下がる。 */
+const Container = ({
+  label,
+  labelSide,
+  rightColumnWidth: propsRightColWidth,
+  indentSizePx: propsIndentSizePx,
+  children,
+  className,
+}: {
+  label?: string
+  labelSide?: React.ReactNode
+  rightColumnWidth?: string
+  indentSizePx?: number
   children?: React.ReactNode
-  hidden?: boolean
+  className?: string
 }) => {
-  const { depth: parentDepth, tableStartDepth } = useContext(NearestSectionContext)
-  const parentIsTable = tableStartDepth !== undefined
-  const thisContextValue: NearestSectionContextValue = useMemo(() => {
-    let currentDepth: number
-    if (!props.label) {
-      currentDepth = parentDepth
-    } else if (!parentIsTable && props.table) {
-      currentDepth = parentDepth
-    } else {
-      currentDepth = parentDepth + 1
-    }
-    return {
-      depth: currentDepth,
-      tableStartDepth: tableStartDepth ?? (props.table ? currentDepth : undefined),
-    }
-  }, [props.label, parentDepth, props.table, tableStartDepth, parentIsTable])
+  const { depth, indentSizePx, rightColumnWidth } = React.useContext(VFormContext)
+  const innerContextValue = React.useMemo<VFormContextValue>(() => ({
+    depth: depth + 1,
+    rightColumnWidth: propsRightColWidth ?? rightColumnWidth,
+    indentSizePx: propsIndentSizePx ?? indentSizePx,
+  }), [depth, propsRightColWidth, rightColumnWidth, propsIndentSizePx, indentSizePx])
+
+  const isRoot = depth === 0
 
   return (
-    <NearestSectionContext.Provider value={thisContextValue}>
-      <div className={`flex flex-row ${props.hidden && 'hidden'}`}>
-        <Indent depth={parentDepth} tableStartDepth={thisContextValue.tableStartDepth} />
-        <div className={parentIsTable
-          ? `flex gap-1 flex-1 text-sm font-semibold p-1 ${BG_COLOR_HEADER} border ${BORDER_COLOR} mt-[-1px]`
-          : `flex gap-1 flex-1 text-sm font-semibold p-1`}>
-          {props.label}
+    <VFormContext.Provider value={innerContextValue}>
+      <div className={`flex flex-col justify-start items-stretch ${className} ${!isRoot ? 'border-t first:border-none border-color-5' : ''}`}>
+        {(label || labelSide) && (
+          <div className={`flex flex-wrap gap-2 items-center ${isRoot ? 'py-1' : 'p-1'}`}>
+            <LabelText>{label}</LabelText>
+            {labelSide}
+          </div>)}
+        <div className="flex-1 flex bg-color-3">
+          {!isRoot && <Indent className="bg-color-3" />}
+          <div className={`flex-1 border-t border-l ${isRoot ? 'border-r border-b' : ''} border-color-5`}>
+            {children}
+          </div>
         </div>
       </div>
-      {props.children}
-    </NearestSectionContext.Provider>
-  )
-})
-
-const Row = ((props: {
-  label?: React.ReactNode
-  fullWidth?: boolean
-  children?: React.ReactNode
-  hidden?: boolean
-}) => {
-  const { depth, tableStartDepth } = useContext(NearestSectionContext)
-
-  return props.fullWidth
-    ? <>
-      {(!props.hidden && props.label) && (
-        <IndentAndLabel fullWidth>
-          {props.label}
-        </IndentAndLabel>
-      )}
-      <div className={`flex flex-row overflow-x-auto ${props.hidden && 'hidden'}`}>
-        <Indent depth={depth} tableStartDepth={tableStartDepth} />
-        <BodyCell table={tableStartDepth !== undefined} fullWidth>
-          {props.children}
-        </BodyCell>
-      </div>
-    </>
-
-    : (
-      <div className={`flex flex-row items-stretch ${props.hidden && 'hidden'}`}>
-        <IndentAndLabel>
-          {props.label}
-        </IndentAndLabel>
-        <BodyCell table={tableStartDepth !== undefined}>
-          {props.children}
-        </BodyCell>
-      </div>
-    )
-})
-
-const BodyCell = ({ table, fullWidth, children }: {
-  table?: boolean
-  fullWidth?: boolean
-  children?: React.ReactNode
-}) => {
-  return (
-    <div className={table
-      ? `bg-color-base flex-1 flex gap-2 p-[1px] border ${BORDER_COLOR} ${!fullWidth && 'ml-[-1px]'} mt-[-1px]`
-      : `bg-color-base flex-1 flex gap-2 p-[1px] `}>
-      {children}
-    </div>
+    </VFormContext.Provider>
   )
 }
 
-
-const IndentAndLabel = ({ fullWidth, children }: {
-  fullWidth?: boolean
+/** 名前と値のペア */
+const Item = ({ label, labelSide, wide, children }: {
+  label?: string
+  labelSide?: React.ReactNode
+  wide?: boolean
   children?: React.ReactNode
 }) => {
-  const { leftColumnWidth } = useContext(FormLayoutContext)
-  const { depth, tableStartDepth } = useContext(NearestSectionContext)
-  return (
-    <div
-      style={{ flexBasis: fullWidth ? undefined : leftColumnWidth }}
-      className="flex flex-row items-stretch"
-    >
-      <Indent depth={depth} tableStartDepth={tableStartDepth} />
-      <div className={tableStartDepth === undefined
-        ? `text-sm font-semibold p-1 flex-1 flex items-center`
-        : `text-sm font-semibold p-1 flex-1 flex items-center border ${BORDER_COLOR} ${BG_COLOR_HEADER} mt-[-1px]`}>
+  const { rightColumnWidth } = React.useContext(VFormContext)
+  const valueColumnStyle: React.CSSProperties = {
+    flex: wide ? '1' : undefined,
+    flexBasis: wide ? undefined : (rightColumnWidth ?? '28rem'),
+  }
+
+  return <>
+    {wide && label && (
+      <div className="flex flex-wrap items-center gap-2 p-1 border-t first:border-none border-color-5">
+        <LabelText>{label}</LabelText>
+        {labelSide}
+      </div >
+    )}
+    <div className="flex align-center border-t first:border-none border-color-5">
+      {!wide && (
+        <div className="flex-1 flex flex-col items-start gap-2 p-1 bg-color-3 border-r border-color-5">
+          <LabelText>{label}</LabelText>
+          {labelSide}
+        </div>
+      )}
+      <div className="p-1 bg-color-0" style={valueColumnStyle}>
         {children}
       </div>
     </div>
+  </>
+}
+const LabelText = ({ children }: {
+  children?: React.ReactNode
+}) => {
+  return children && (
+    <span className="select-none text-color-7">
+      {children}
+    </span>
   )
 }
 
-const Indent = ({ depth, tableStartDepth }: {
-  depth: number
-  tableStartDepth: number | undefined
+const Indent = ({ className }: {
+  className?: string
 }) => {
-  const { indentSizePx } = useContext(FormLayoutContext)
+  const { indentSizePx } = React.useContext(VFormContext)
 
-  return <>
-    {Array.from({ length: depth }).map((_, index) => (
-      <div
-        key={index}
-        style={{ width: `${indentSizePx}px` }}
-        className={(tableStartDepth !== undefined && index >= tableStartDepth)
-          ? `inline-block ${BG_COLOR_HEADER} border-l border-b ${BORDER_COLOR} mt-[-1px]`
-          : `inline-block`}
-      >
-      </div>
-    ))}
-  </>
+  return (
+    <div
+      className={className}
+      style={{ flexBasis: indentSizePx ?? 24 }}
+    ></div>
+  )
 }
 
-const Spacer = ({ large }: {
-  large?: boolean
-}) => {
-  const className = large ? 'm-6' : 'm-3'
-  return <div className={className}></div>
-}
-// ----------------------------------------
-const BG_COLOR_HEADER = 'bg-color-3'
-const BORDER_COLOR = 'border-color-5'
-
-// ----------------------------------------
-
-type RootContextValue = {
-  leftColumnWidth?: string
+type VFormContextValuePublic = {
+  rightColumnWidth?: string
   indentSizePx?: number
 }
-const FormLayoutContext = React.createContext<RootContextValue>({})
-
-// ----------------------------------------
-
-type NearestSectionContextValue = {
+type VFormContextValue = VFormContextValuePublic & {
   depth: number
-  tableStartDepth: number | undefined
 }
-const NearestSectionContext = React.createContext<NearestSectionContextValue>({
+const VFormContext = React.createContext<VFormContextValue>({
   depth: 0,
-  tableStartDepth: undefined,
 })
 
-// ----------------------------------------
-
 export const VerticalForm = {
-  Root,
-  Section,
-  Row,
-  Spacer,
+  Container,
+  Item,
 }
