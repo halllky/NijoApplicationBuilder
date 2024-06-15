5275363bf90a838e0bc22b3470ee2c810e068cca
halky <yuyami.stars@gmail.com>
2024-06-15 12:13:02 +0900

    TextareaBaseを通常のものに戻す

--- Nijo.ApplicationTemplates/REACT_AND_WEBAPI/react/src/__autoGenerated/input/TextareaBase.tsx
+++ Nijo.ApplicationTemplates/REACT_AND_WEBAPI/react/src/__autoGenerated/input/TextareaBase.tsx
@@ -1,9 +1,60 @@
-import React, { HTMLAttributes, useCallback, useImperativeHandle, useRef } from 'react'
+import React, { HTMLAttributes, TextareaHTMLAttributes, useCallback, useImperativeHandle, useRef } from 'react'
 import SimpleMDE, { GetMdeInstance } from 'react-simplemde-editor'
 import 'easymde/dist/easymde.min.css'
 import { defineCustomComponent } from './InputBase'
 
-export const TextareaBase = defineCustomComponent<string, {}, HTMLAttributes<HTMLDivElement>>((props, ref) => {
+/**
+ * HTML標準のシンプルなテキストエリア
+ */
+export const TextareaBase = defineCustomComponent<string, {}, TextareaHTMLAttributes<HTMLTextAreaElement>>((props, ref) => {
+  const {
+    onChange,
+    value,
+    readOnly,
+    onFocus,
+    rows,
+    className,
+    ...rest
+  } = props
+
+  const textareaRef = useRef<HTMLTextAreaElement>(null)
+
+  useImperativeHandle(ref, () => ({
+    getValue: () => textareaRef.current?.value,
+    focus: () => textareaRef.current?.focus(),
+  }), [])
+
+  const handleFocus: React.FocusEventHandler<HTMLTextAreaElement> = useCallback(e => {
+    if (readOnly) textareaRef.current?.select()
+    onFocus?.(e)
+  }, [onFocus, readOnly])
+  const onTextChange: React.ChangeEventHandler<HTMLTextAreaElement> = useCallback(e => {
+    onChange?.(e.target.value)
+  }, [onChange])
+
+  return (
+    <textarea
+      {...rest}
+      ref={textareaRef}
+      value={value ?? ''}
+      className={readOnly
+        ? `block w-full outline-none px-1 border border-color-4 bg-transparent cursor-default ${className}`
+        : `block w-full outline-none px-1 border border-color-5 bg-color-base  ${className}`}
+      autoComplete="off"
+      spellCheck={false}
+      rows={rows || 3}
+      readOnly={readOnly}
+      onFocus={handleFocus}
+      onChange={onTextChange}
+    />
+  )
+})
+
+
+/**
+ * react-simplemde-editor によるリッチなテキストエリア
+ */
+export const SimpleMdeTextareaBase = defineCustomComponent<string, {}, HTMLAttributes<HTMLDivElement>>((props, ref) => {
 
   const {
     onChange,
