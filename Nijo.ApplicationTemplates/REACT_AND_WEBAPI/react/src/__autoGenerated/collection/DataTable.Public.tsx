082017c421644d42de7a3779ccd3d612b1d87696
halky <yuyami.stars@gmail.com>
2024-06-16 15:00:31 +0900

    typo

--- Nijo.ApplicationTemplates/REACT_AND_WEBAPI/react/src/__autoGenerated/collection/DataTable.Public.tsx
+++ Nijo.ApplicationTemplates/REACT_AND_WEBAPI/react/src/__autoGenerated/collection/DataTable.Public.tsx
@@ -20,12 +20,12 @@ export type ColumnDefEx<TRow> = RT.ColumnDef<TRow> & {
 
 export type ColumnEditSetting<TRow, TOption = unknown> = {
   readOnly?: ((row: TRow) => boolean)
-} & (TextColumndEditSetting<TRow>
+} & (TextColumnEditSetting<TRow>
   | TextareaColumndEditSetting<TRow>
   | SyncComboColumnEditSetting<TRow, TOption>
   | AsyncComboColumnEditSetting<TRow, TOption>)
 
-type TextColumndEditSetting<TRow> = {
+type TextColumnEditSetting<TRow> = {
   type: 'text'
   getTextValue: (row: TRow) => string | undefined
   setTextValue: (row: TRow, value: string | undefined) => void
