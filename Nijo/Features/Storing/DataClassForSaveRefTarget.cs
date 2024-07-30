ba16c78768d6bd41f58ddbaf7162743ca450cd96
halky <yuyami.stars@gmail.com>
2024-07-30 22:54:01 +0900

    fix 参照先のキーにint型が含まれるときエラーになるのを修正

--- Nijo/Features/Storing/DataClassForSaveRefTarget.cs
+++ Nijo/Features/Storing/DataClassForSaveRefTarget.cs
@@ -76,7 +76,7 @@ namespace Nijo.Features.Storing {
                 foreach (var m in new DataClassForSaveRefTarget(agg).GetOwnMembers()) {
                     if (m is AggregateMember.ValueMember vm) {
                         yield return $$"""
-                            {{vm.MemberName}} = ({{vm.CSharpTypeName}}?){{vmKeys.Single(x => x.Member.Declared == vm.Declared).VarName}},
+                            {{vm.MemberName}} = {{vmKeys.Single(x => x.Member.Declared == vm.Declared).VarName}},
                             """;
 
                     } else if (m is AggregateMember.RelationMember rm) {
@@ -89,6 +89,40 @@ namespace Nijo.Features.Storing {
                 }
             }
 
+            string ConvertOrThrow(string element, string varName, string csharpTypeName, string memberName) {
+                if (csharpTypeName == "string") {
+                    return $$"""
+                        var {{varName}} = {{element}}.GetString();
+                        """;
+
+                } else if (csharpTypeName == "int") {
+                    return $$"""
+                        if (!{{element}}.TryGetInt32(out var {{varName}}))
+                            throw new InvalidOperationException($"{{_aggregate.Item.DisplayName}}のキーの変換に失敗しました。{{memberName}}の位置の値が{{csharpTypeName}}型ではありません: {{{element}}.GetString()}");
+                        """;
+
+                } else if (csharpTypeName == "decimal") {
+                    return $$"""
+                        if (!{{element}}.TryGetDecimal(out var {{varName}}))
+                            throw new InvalidOperationException($"{{_aggregate.Item.DisplayName}}のキーの変換に失敗しました。{{memberName}}の位置の値が{{csharpTypeName}}型ではありません: {{{element}}.GetString()}");
+                        """;
+
+                } else if (csharpTypeName == "DateTime") {
+                    return $$"""
+                        if (!{{element}}.TryGetDateTime(out var {{varName}}))
+                            throw new InvalidOperationException($"{{_aggregate.Item.DisplayName}}のキーの変換に失敗しました。{{memberName}}の位置の値が{{csharpTypeName}}型ではありません: {{{element}}.GetString()}");
+                        """;
+
+                } else if (csharpTypeName == "bool") {
+                    return $$"""
+                        var {{varName}} = {{element}}.GetBoolean();
+                        """;
+
+                } else {
+                    throw new NotImplementedException();
+                }
+            }
+
             return $$"""
                 /// <summary>
                 /// <see cref="{{CSharpClassName}}"/> 型のプロパティの値が
@@ -98,12 +132,13 @@ namespace Nijo.Features.Storing {
                     public override {{CSharpClassName}}? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) {
                         var jsonArray = reader.GetString();
                         if (jsonArray == null) return null;
-                        var objArray = Util.{{UtilityClass.PARSE_JSON_AS_OBJARR}}(jsonArray);
+                        var objArray = Util.{{UtilityClass.PARSE_JSON}}<JsonElement[]>(jsonArray);
+
+                        if (objArray.Length != {{vmKeys.Length}})
+                            throw new InvalidOperationException($"{{_aggregate.Item.DisplayName}}のキーの変換に失敗しました。キーの数は{{vmKeys.Length}}個であるべきところ、実際は{objArray.Length}個でした。");
 
                 {{vmKeys.SelectTextTemplate(x => $$"""
-                        var {{x.VarName}} = objArray.ElementAtOrDefault({{x.Index}});
-                        if ({{x.VarName}} != null && {{x.VarName}} is not {{x.Member.CSharpTypeName}})
-                            throw new InvalidOperationException($"{{CSharpClassName}}の値の変換に失敗しました。{{x.Member.MemberName}}の位置の値が{{x.Member.CSharpTypeName}}型ではありません: {{{x.VarName}}}");
+                        {{WithIndent(ConvertOrThrow($"objArray[{x.Index}]", x.VarName, x.Member.CSharpTypeName, x.Member.MemberName), "        ")}}
 
                 """)}}
                         return new {{CSharpClassName}} {
