72b0f4be59cbcf7b71294e2bb309a3b5fac4c736
halky <yuyami.stars@gmail.com>
2024-08-31 23:00:29 +0900

    React router :  公式推奨の data router の実装に変更

--- Nijo.ApplicationTemplate/react/src/__autoGenerated/autogenerated-menu.tsx
+++ Nijo.ApplicationTemplate/react/src/__autoGenerated/autogenerated-menu.tsx
@@ -2,7 +2,9 @@
  * このファイルはソース自動生成によって上書きされます。
  */
 
+import { RouteObject } from 'react-router-dom'
+
 export const THIS_APPLICATION_NAME = 'サンプルアプリケーション' as const
-export const routes: { url: string, el: JSX.Element }[] = []
+export const routes: RouteObject[] = []
 export const menuItems: { url: string, text: string }[] = []
 export const SHOW_LOCAL_REPOSITORY_MENU = true
