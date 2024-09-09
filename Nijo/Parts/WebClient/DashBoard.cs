8f25fd2572f8338a1a90a10dbbaf73e39ef54fc8
halky <yuyami.stars@gmail.com>
2024-09-10 07:18:39 +0900

    デバッグ時に接続先データベース表示

--- Nijo/Parts/WebClient/DashBoard.cs
+++ Nijo/Parts/WebClient/DashBoard.cs
@@ -12,11 +12,12 @@ namespace Nijo.Parts.WebClient {
             return new SourceFile {
                 FileName = "DashBoard.tsx",
                 RenderContent = context => $$"""
-                    import { useCallback, useState } from 'react'
+                    import { useCallback, useState, useEffect } from 'react'
                     import { useEvent } from 'react-use-event-hook'
+                    import * as Icon from '@heroicons/react/24/outline'
                     import * as Util from '../util'
                     import * as Input from '../input'
-                    import { VerticalForm as VForm } from '../collection'
+                    import { VForm2 } from '../collection'
                     {{context.ReactProject.DashBoardImports.SelectTextTemplate(import => import)}}
 
                     /** DashBoard */
@@ -33,7 +34,7 @@ namespace Nijo.Parts.WebClient {
                       // デバッグ用DB再作成コマンド
                       const [, dispatchMsg] = Util.useMsgContext()
                       const [, dispatchToast] = Util.useToastContext()
-                      const { post } = Util.useHttpRequest()
+                      const { get, post } = Util.useHttpRequest()
                       const [withDummyData, setWithDummyData] = useState<boolean | undefined>(true)
                       const genereateDummyData2 = Util.useDummyDataGenerator2()
                       const { reset: resetLocalRepository } = Util.useLocalRepositoryChangeList()
@@ -59,6 +60,24 @@ namespace Nijo.Parts.WebClient {
                         }
                       })
 
+                      // 現在の接続先DB
+                      const [currentDb, setCurrentDb] = useState<{ name?: string, connStr?: string } | undefined>()
+                      useEffect(() => {
+                        get<{ currentDb?: string, db?: { name?: string, connStr?: string }[] }>('/WebDebugger/secret-settings').then(res => {
+                          if (!res.ok) return
+                          const currentDbDetail = res.data.db?.find(x => x.name === res.data.currentDb)
+                          if (currentDbDetail) {
+                            setCurrentDb(currentDbDetail)
+                          } else {
+                            setCurrentDb({ name: res.data?.currentDb })
+                          }
+                        })
+                      }, [])
+                      const [visible, toggleVisigle] = Util.useToggle()
+                      const handleToggle = useEvent(() => {
+                        toggleVisigle(x => x.toggle())
+                      })
+
                       return (
                         <div className="page-content-root gap-4 p-1">
                           <div className="flex gap-1">
@@ -69,12 +88,29 @@ namespace Nijo.Parts.WebClient {
                           <Util.InlineMessageList />
 
                           {import.meta.env.DEV && (
-                            <VForm.Container label="デバッグ用コマンド ※この欄は開発環境でのみ表示されます">
-                              <VForm.Item label="データベース">
-                                <Input.Button outlined onClick={recreateDatabase}>DBを再作成する</Input.Button>
-                                <Input.CheckBox value={withDummyData} onChange={setWithDummyData}>ダミーデータも併せて作成する</Input.CheckBox>
-                              </VForm.Item>
-                            </VForm.Container>
+                            <VForm2.Root label="デバッグ用コマンド ※この欄は開発環境でのみ表示されます" estimatedLabelWidth="8rem">
+                              <VForm2.Indent label="データベース">
+                                <VForm2.Item label="再作成" wideValue>
+                                  <Input.IconButton onClick={recreateDatabase} fill>DBを再作成する</Input.IconButton>
+                                </VForm2.Item>
+                                <VForm2.Item wideValue>
+                                  <Input.CheckBox value={withDummyData} onChange={setWithDummyData}>ダミーデータも併せて作成する</Input.CheckBox>
+                                </VForm2.Item>
+                                <VForm2.Indent label="現在の接続先データベース（{{RuntimeSettings.JSON_FILE_NAME}} で設定されています）">
+                                  <VForm2.Item label="設定名" wideValue>
+                                    {currentDb?.name}
+                                  </VForm2.Item>
+                                  <VForm2.Item label="接続文字列" wideValue>
+                                    <div className="flex justify-start gap-1">
+                                      <Input.IconButton icon={(visible ? Icon.EyeIcon : Icon.EyeSlashIcon)} onClick={handleToggle} outline mini>表示</Input.IconButton>
+                                      <span>
+                                        {(visible ? currentDb?.connStr : '*********************************')}
+                                      </span>
+                                    </div>
+                                  </VForm2.Item>
+                                </VForm2.Indent>
+                              </VForm2.Indent>
+                            </VForm2.Root>
                           )}
 
                     {{context.ReactProject.DashBoardContents.SelectTextTemplate(source => $$"""
