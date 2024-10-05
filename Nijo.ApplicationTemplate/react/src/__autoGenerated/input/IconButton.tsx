a887f8648fc261e36553c0636b222867715b5649
halky <yuyami.stars@gmail.com>
2024-10-05 11:43:45 +0900

    #51 IconButtonにloading属性追加

--- Nijo.ApplicationTemplate/react/src/__autoGenerated/input/IconButton.tsx
+++ Nijo.ApplicationTemplate/react/src/__autoGenerated/input/IconButton.tsx
@@ -7,6 +7,7 @@ export const IconButton = (args: {
   outline?: boolean
   underline?: boolean
   mini?: boolean
+  loading?: boolean
   hideText?: boolean
   icon?: React.ElementType
   iconRight?: boolean
@@ -18,34 +19,64 @@ export const IconButton = (args: {
 
   const flex = args.inline ? 'inline-flex' : 'flex'
   const direction = args.iconRight ? 'flex-row-reverse' : 'flex-row'
-  const padding = args.mini ? 'px-1 py-px' : 'px-2 py-1'
-  let className: string
+
+  let className = `${flex} ${direction} justify-center items-center gap-1 select-none outline-none ${args.className}`
+
+  if (args.fill || args.outline) {
+    className += (args.mini ? ' px-1 py-px' : ' px-2 py-1')
+  }
+
   if (args.fill) {
-    className = `${flex} ${direction} justify-center items-center select-none outline-none ${args.className} gap-1 ${padding} text-color-0 bg-color-button`
+    // 普通に text-color-8 などとするとTailwind既定のスタイルリセットに優先度で負けるので背景色は独自クラスを指定
+    className += args.loading
+      ? ` text-color-0 bg-color-button-loading`
+      : ` text-color-0 bg-color-button`
   } else if (args.outline) {
-    className = `${flex} ${direction} justify-center items-center select-none outline-none ${args.className} gap-1 ${padding} border border-color-7`
+    className += args.loading
+      ? ` border border-color-5 text-color-5`
+      : ` border border-color-7`
   } else if (args.underline) {
-    className = `${flex} ${direction} justify-center items-center select-none outline-none ${args.className} pr-1 text-blue-600 border-b border-blue-500`
+    className += args.loading
+      ? ` pr-1 text-sky-300 border-b border-sky-300`
+      : ` pr-1 text-sky-600 border-b border-sky-600`
+  }
+
+  // 読み込み中のくるくる
+  let nowLoadingStyle = `animate-spin h-4 w-4 border-2 rounded-full border-t-transparent`
+  if (args.fill) {
+    nowLoadingStyle += ' border-color-0'
+  } else if (args.underline) {
+    nowLoadingStyle += ' border-sky-300'
   } else {
-    className = `${flex} ${direction} justify-center items-center select-none outline-none ${args.className} gap-1`
+    nowLoadingStyle += ' border-color-5'
   }
 
   return (
     <button
       type={args.submit ? undefined : 'button'}
-      onClick={args.onClick}
+      onClick={args.loading ? undefined : args.onClick}
       className={className}
       title={args.hideText ? (args.children as string) : undefined}
       tabIndex={args.tabIndex}
     >
 
-      {args.icon && React.createElement(args.icon, { className: 'w-4' })}
+      {/* アイコン or 読み込み中のくるくる */}
+      {args.icon && !args.loading && (
+        React.createElement(args.icon, { className: 'w-4' })
+      )}
+      {args.icon && args.loading && (
+        <div className={nowLoadingStyle}></div>
+      )}
 
+      {/* テキスト */}
       {!args.hideText && args.children && (
-        <span className="text-sm whitespace-nowrap">
+        <span className={`text-sm whitespace-nowrap relative ${(args.loading && !args.icon ? '' : '')}`}>
           {args.children}
+          {args.loading && !args.icon && (
+            <div className={`absolute inset-0 m-auto ${nowLoadingStyle}`}></div>
+          )}
         </span>
       )}
-    </button>
+    </button >
   )
 }
