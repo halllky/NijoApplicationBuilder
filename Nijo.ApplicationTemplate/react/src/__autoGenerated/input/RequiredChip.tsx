fd0a3e337079fb1760854fe7c7e565558123fb31
halky <yuyami.stars@gmail.com>
2024-10-06 15:25:55 +0900

    #46 必須入力チップの見た目修正

--- Nijo.ApplicationTemplate/react/src/__autoGenerated/input/RequiredChip.tsx
+++ Nijo.ApplicationTemplate/react/src/__autoGenerated/input/RequiredChip.tsx
@@ -1,12 +1,8 @@
-import React from 'react'
-
 /** 必須入力項目であることを表すUI */
-export const RequiredChip = React.memo(({ className }: {
-  className?: string
-}) => {
+export const RequiredChip = () => {
   return (
-    <span className={`p-1 select-none text-xs text-color-5 bg-color-3 font-bold whitespace-nowrap rounded-md ${className ?? ''}`}>
-      必須
+    <span className="select-none text-error font-bold">
+      *
     </span>
   )
-})
+}
