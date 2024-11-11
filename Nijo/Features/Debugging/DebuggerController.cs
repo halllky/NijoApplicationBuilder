0c38c06cf32f1f03e59daacef0bbece72523f6c6
halky <yuyami.stars@gmail.com>
2024-11-11 11:41:59 +0900

    インデントずれ修正

--- Nijo/Features/Debugging/DebuggerController.cs
+++ Nijo/Features/Debugging/DebuggerController.cs
@@ -23,36 +23,36 @@ namespace Nijo.Features.Debugging {
                 [ApiController]
                 [Route("[controller]")]
                 public class WebDebuggerController : ControllerBase {
-                  public WebDebuggerController(ILogger<WebDebuggerController> logger, IServiceProvider provider) {
-                    _logger = logger;
-                    _provider = provider;
-                  }
-                  private readonly ILogger<WebDebuggerController> _logger;
-                  private readonly IServiceProvider _provider;
+                    public WebDebuggerController(ILogger<WebDebuggerController> logger, IServiceProvider provider) {
+                        _logger = logger;
+                        _provider = provider;
+                    }
+                    private readonly ILogger<WebDebuggerController> _logger;
+                    private readonly IServiceProvider _provider;
 
-                  [HttpPost("recreate-database")]
-                  public HttpResponseMessage RecreateDatabase() {
-                    var dbContext = _provider.GetRequiredService<{{ctx.Config.DbContextNamespace}}.{{ctx.Config.DbContextName}}>();
-                    dbContext.Database.EnsureDeleted();
-                    dbContext.Database.EnsureCreated();
-                    return new HttpResponseMessage {
-                      StatusCode = System.Net.HttpStatusCode.OK,
-                      Content = new StringContent("DBを再作成しました。"),
-                    };
-                  }
+                    [HttpPost("recreate-database")]
+                    public HttpResponseMessage RecreateDatabase() {
+                        var dbContext = _provider.GetRequiredService<{{ctx.Config.DbContextNamespace}}.{{ctx.Config.DbContextName}}>();
+                        dbContext.Database.EnsureDeleted();
+                        dbContext.Database.EnsureCreated();
+                        return new HttpResponseMessage {
+                            StatusCode = System.Net.HttpStatusCode.OK,
+                            Content = new StringContent("DBを再作成しました。"),
+                        };
+                    }
 
-                  [HttpGet("secret-settings")]
-                  public IActionResult GetSecretSettings() {
-                    var runtimeSetting = _provider.GetRequiredService<{{RuntimeSettings.ServerSetiingTypeFullName}}>();
-                    return this.JsonContent(runtimeSetting);
-                  }
-                  [HttpPost("secret-settings")]
-                  public IActionResult SetSecretSettings([FromBody] {{RuntimeSettings.ServerSetiingTypeFullName}} settings) {
-                    var json = settings.{{RuntimeSettings.TO_JSON}}();
-                    using var sw = new System.IO.StreamWriter("{{RuntimeSettings.JSON_FILE_NAME}}", false, new System.Text.UTF8Encoding(false));
-                    sw.WriteLine(json);
-                    return Ok();
-                  }
+                    [HttpGet("secret-settings")]
+                    public IActionResult GetSecretSettings() {
+                        var runtimeSetting = _provider.GetRequiredService<{{RuntimeSettings.ServerSetiingTypeFullName}}>();
+                        return this.JsonContent(runtimeSetting);
+                    }
+                    [HttpPost("secret-settings")]
+                    public IActionResult SetSecretSettings([FromBody] {{RuntimeSettings.ServerSetiingTypeFullName}} settings) {
+                        var json = settings.{{RuntimeSettings.TO_JSON}}();
+                        using var sw = new System.IO.StreamWriter("{{RuntimeSettings.JSON_FILE_NAME}}", false, new System.Text.UTF8Encoding(false));
+                        sw.WriteLine(json);
+                        return Ok();
+                    }
                 }
                 #endif
                 """,
