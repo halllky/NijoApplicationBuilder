5b94b14d2a8f26fbfebe35e04c5ad094f6aa4baf
halky <yuyami.stars@gmail.com>
2024-12-20 15:21:49 +0900

    テスト017が落ちていたのを修正

--- Nijo.IntegrationTest/DataPatternsClass/017_CommandModel.cs
+++ Nijo.IntegrationTest/DataPatternsClass/017_CommandModel.cs
@@ -10,10 +10,10 @@ namespace Nijo.IntegrationTest.DataPatternsClass {
 
         protected override string OverridedAppSrvMethods() {
             return $$"""
-                public override ICommandResult Execute従業員データ一括取り込み(従業員データ一括取り込みParameter param, ICommandResultGenerator<従業員データ一括取り込みParameterMessages> result) {
+                public override Task<ICommandResult> Execute従業員データ一括取り込み(従業員データ一括取り込みParameter param, ICommandResultGenerator<従業員データ一括取り込みParameterMessages> result) {
                     if (param.処理範囲 == E_処理範囲.特定データのみ) {
                         if (param.特定データのみ?.従業員?.内部ID == null) {
-                            return result.Error("対象の従業員が指定されていません。");
+                            return Task.FromResult(result.Error("対象の従業員が指定されていません。"));
                         }
                         var searchResult = Load従業員(new() {
                             Filter = new() {
@@ -24,22 +24,22 @@ namespace Nijo.IntegrationTest.DataPatternsClass {
                         }).ToArray();
 
                         if (searchResult.Length == 0) {
-                            return result.Error("従業員データが見つかりません。");
+                            return Task.FromResult(result.Error("従業員データが見つかりません。"));
                         }
 
                         var displayData = searchResult[0];
                         displayData.Values.名前 += "あ"; // リダイレクト先の編集画面では、名前を書き換えた状態の初期値が表示される
-                        return result.Redirect(displayData, E_SingleViewType.Edit, E_RefetchType.Overwrite);
+                        return Task.FromResult(result.Redirect(displayData, E_SingleViewType.Edit, E_RefetchType.Overwrite));
 
                     } else {
-                        return result.Ok("オッケーです");
+                        return Task.FromResult(result.Ok("オッケーです"));
 
                         //// テキストファイルの内容を生成
                         //var content = "これはテキストファイルの内容です。\n行1\n行2\n行3";
                         //var bytes = Encoding.UTF8.GetBytes(content);
 
                         //// Fileメソッドを使用してファイルを返す
-                        //return result.File(bytes, "text/plain");
+                        //return Task.FromResult(result.File(bytes, "text/plain"));
                     }
                 }
                 """;
