446c276e58903d76d4f8978ca847e09afc174458
halky <yuyami.stars@gmail.com>
2025-04-20 17:21:43 +0900

    TailwindCSS: 公式(v4.1)の記載に従ってインストールしなおし

--- Nijo.ApplicationTemplate.Ver1/react/src/App.tsx
+++ Nijo.ApplicationTemplate.Ver1/react/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="h-full bg-gray-100 flex flex-col justify-center">
+    <div className="h-full bg-gray-100 flex flex-col justify-center items-center">
       <h1 className="text-3xl font-bold text-gray-900 mb-5">Tailwind CSSが導入されました</h1>
       <p className="text-gray-600">美しいUIコンポーネントを簡単に作成できます</p>
     </div>
