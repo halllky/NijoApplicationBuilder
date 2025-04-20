90a98abfe389983c32e48ef68b5fecc9ca96e347
halky <yuyami.stars@gmail.com>
2025-04-20 16:26:07 +0900

    tailwind導入

--- Nijo.ApplicationTemplate.Ver1/react/src/App.tsx
+++ Nijo.ApplicationTemplate.Ver1/react/src/App.tsx
@@ -1,34 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="h-full bg-gray-100 flex flex-col justify-center">
+      <h1 className="text-3xl font-bold text-gray-900 mb-5">Tailwind CSSが導入されました</h1>
+      <p className="text-gray-600">美しいUIコンポーネントを簡単に作成できます</p>
+    </div>
   )
 }
 
