692f99a5beb9cf58bf73cdff73965955ac78b290
halky <yuyami.stars@gmail.com>
2025-05-06 10:23:16 +0900

    古いソースをコメントアウト

--- Nijo.ApplicationTemplate.Ver1/react/src/pages/index.tsx
+++ Nijo.ApplicationTemplate.Ver1/react/src/pages/index.tsx
@@ -1,17 +1,17 @@
 import { RouteObjectWithSideMenuSetting } from "../routes";
-import { 従業員一覧検索 } from "./従業員/従業員一覧検索";
-import { 従業員詳細編集 } from "./従業員/従業員詳細編集";
-import { 顧客一覧検索 } from "./顧客/顧客一覧検索";
-import { 顧客詳細編集 } from "./顧客/顧客詳細編集";
+
+// *** 実装例 ここから ***
+// import { 従業員一覧検索 } from "./【実装例サンプル】従業員/従業員一覧検索";
+// import { 従業員詳細編集 } from "./【実装例サンプル】従業員/従業員詳細編集";
+// *** 実装例 ここまで ***
 
 /** Home以外の画面のルーティング設定 */
 export default function (): RouteObjectWithSideMenuSetting[] {
   return [
-    { path: '顧客', element: <顧客一覧検索 />, sideMenuLabel: "顧客一覧" },
-    { path: '顧客/new', element: <顧客詳細編集 /> },
-    { path: '顧客/:顧客ID', element: <顧客詳細編集 /> },
-    { path: '従業員', element: <従業員一覧検索 />, sideMenuLabel: "従業員一覧" },
-    { path: '従業員/new', element: <従業員詳細編集 /> },
-    { path: '従業員/:従業員ID', element: <従業員詳細編集 /> },
+    // *** 実装例 ここから ***
+    // { path: '従業員', element: <従業員一覧検索 />, sideMenuLabel: "従業員一覧" },
+    // { path: '従業員/new', element: <従業員詳細編集 /> },
+    // { path: '従業員/:従業員ID', element: <従業員詳細編集 /> },
+    // *** 実装例 ここまで ***
   ]
 }
