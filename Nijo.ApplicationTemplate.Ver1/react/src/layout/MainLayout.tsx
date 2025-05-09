5dbd079adc43c4942a597709a8493ae841670877
halky <yuyami.stars@gmail.com>
2025-05-10 08:27:58 +0900

    リファクタリング: ファイル移動

--- Nijo.ApplicationTemplate.Ver1/react/src/layout/MainLayout.tsx
+++ Nijo.ApplicationTemplate.Ver1/react/src/layout/MainLayout.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Sidebar from '../components/Sidebar'
+import Sidebar from './MainLayout.Sidebar'
 
 const MainLayout: React.FC = () => {
   return (
