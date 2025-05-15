2e8fb3c8a607b1433319015e30e2e66af23ca4b6
halky <yuyami.stars@gmail.com>
2025-05-16 07:49:47 +0900

    style

--- Nijo.ApplicationTemplate.Ver1/react/src/main.tsx
+++ Nijo.ApplicationTemplate.Ver1/react/src/main.tsx
@@ -1,7 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './App.css'
 import App from './App.tsx'
+
+// CSSの読み込み
+import './App.css'
+if (import.meta.env.mode === 'nijo-ui') {
+  import('./App.NijoUi.css')
+} else {
+  import('./App.ReflectionPage.css')
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
